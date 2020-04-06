00e026003800d1259015a3c07a8c747d37746987
Dip <dipbd1@gmail.com>
2020-04-06 23:42:44 +0600

    Add High and Low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high today is '+ body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow +' and There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
